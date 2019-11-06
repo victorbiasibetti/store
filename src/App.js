a42630d5f362b4640f7452661fd90d9ddeda8cb2
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-11-05 21:46:08 -0300

    create a reducers of cart and remove from cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 import store from './store';
 
